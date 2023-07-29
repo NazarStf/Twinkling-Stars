64060707d7c520f37c5c09302e0e13de1800ecd3
NazarStf <hworker98@gmail.com>
2023-07-29 13:53:17 +0300

    Part 1 - Setting up - Fill out the path function

--- Twinkling Stars/ContentView.swift
+++ Twinkling Stars/ContentView.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State private var animateYellow = false
+	@State private var animateBlue = false
+	@State private var animateRed = false
+	@State private var animatePurple = false
+	@State private var animateGreen = false
+	@State private var starThickness: CGFloat = 0.0
+	var starPoints: Int = 0
+	
+	var body: some View {
+		ZStack {
+			//MARK: - BACKGROUND IMAGE
+			Image("stars").resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.all)
+			
+			VStack {
+				//change the amount of points to create a sun effect
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
