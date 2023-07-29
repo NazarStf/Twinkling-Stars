48e2146785cfc8857d693349c908a0dcdecc36e1
NazarStf <hworker98@gmail.com>
2023-07-29 13:48:47 +0300

    Initial Commit

---
+++ Twinkling Stars/Twinkling_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twinkling_StarsApp.swift
+//  Twinkling Stars
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Twinkling_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
